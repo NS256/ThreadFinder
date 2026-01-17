550cc15bb01eecdb34ae6b65a3aa61ab8f78e0cf
Nathan Smith <nls999@icloud.com>
2026-01-17 21:52:05 +0000

    Added documentation for the UnitContext to explain how this works

--- client/src/contexts/UnitContext.jsx
+++ client/src/contexts/UnitContext.jsx
@@ -1,3 +1,12 @@
+/**Context description:
+ * The UnitContext is a boolean value.
+ * This represents whetehr to show data as stored on the server or convert to inches.
+ * 
+ * At a basic level:
+ * - True: MM
+ * - False: Inches
+ */
+
 import React, { createContext, useContext, useState } from 'react';
 
 const UnitContext = createContext();

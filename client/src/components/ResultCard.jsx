604177f801bf9155d0415fe9ca444468ad8337f7
Nathan Smith <nls999@icloud.com>
2026-01-28 21:01:20 +0000

    Created initial card layout. Still needs adjusting but it's enough to continue with the search section build

--- client/src/components/ResultCard.jsx
+++ client/src/components/ResultCard.jsx
@@ -6,13 +6,14 @@ import "../styles/ResultsCard.css";
 export default function ResultCard({thread}) {
     return (
         <div className='thread-card card'>
-            <div className='thread-title title-container'>
+            <div className='thread-title title-container card-inner'>
                 <h3>{`${thread.name} ${thread.family.name ?? ''}`}</h3>
-                {thread.family.fullName && <h4>{thread.family.fullName}</h4>}
+                {thread.family.fullName && <h4 className='thread-family-subtitle'>{thread.family.fullName}</h4>}
             </div>
-            <div className='details thread-details details-container'>
+            <div className='details thread-details details-container card-inner'>
                 <MeasurementRow title="Outer Diameter" measurement={thread.outerDiameter} />
                 <MeasurementRow title="Inner Diameter" measurement={thread.innerDiameter} />
+                <MeasurementRow title="TPI" measurement={thread.tpi} />
                 <MeasurementRow title="Tap Drill Diameter" measurement={thread.tap} />
                 <MeasurementRow title="Clearance Drill Diameter" measurement={thread.clearance} />
             </div>
@@ -21,6 +22,7 @@ export default function ResultCard({thread}) {
 }
 
 function MeasurementRow({title, measurement}) {
+    //unit is boolean - true for metric, false for imperial
     const {unit} = useUnit();
 
     if (!measurement || isNaN(measurement)) {
@@ -28,13 +30,13 @@ function MeasurementRow({title, measurement}) {
     }
 
     return (
-        <div className='measurement-row measurement-container'>
-            <div className='measurement-title-container'>
-                <h5 className='measurement-title'>{title}</h5>
+        <div className='measurement-row'>
+            <div className='measurement-title-container measurement-container'>
+                <h5 className='measurement-title'>{title}:</h5>
             </div>
-            <div className='measurement-value-container'>
+            <div className='measurement-value-container measurement-container'>
                 <p className='measurement-value'>
-                    {(unit === 'mm') ? measurement : (measurement / 25.4)}
+                    {(unit) ? measurement : (measurement / 25.4)}
                 </p>
             </div>
         </div>
